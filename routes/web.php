538cbabb7a0b08d4ccf49488fc4aab6a706c4fe4
cylee1668 <cylee1668@gmail.com>
2025-12-10 16:01:36 +0800

    section 55 end, learned add review

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect('books');
 });
 
-Route::resource('books', BookController::class);
+Route::resource('books', BookController::class)
+    ->only(['index', 'show']); //limited only using these Route
+
+
+Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only(['create','store']);
